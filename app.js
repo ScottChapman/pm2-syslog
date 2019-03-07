fac6f61f1366a2cea582a97b06f6e28906a4a3df
Scott Chapman <scottchapman@us.ibm.com>
2019-03-07 13:47:43 -0500

    log application names as tags

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 
-var pm2       = require('pm2');
+// var pm2       = require('pm2');
+const EventEmitter = require('events')
+class Bus extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+
+class PM2 {
+  constructor() {
+    this.bus = new Bus();
+  }
+  launchBus(cb) {
+    cb(null,this.bus);
+  }
+  bus() {
+    return this.bus
+  }
+}
+
+var pm2 = new PM2();
+
+var os = require('os')
 var SysLogger = require('ain2');
-var logger    = new SysLogger({tag: 'pm2',  facility: 'syslog'});
+var logger    = new SysLogger({tag: 'pm2',  facility: 'local1', hostname: os.hostname()});
+
+var loggers = {};
+
+function getLogger(data) {
+    var name = data.process.name + "(" + data.process.pm_id + ")";
+    if (!loggers.hasOwnProperty(name)) {
+      loggers[name] = new SysLogger({tag: name,  facility: 'local1', hostname: os.hostname()});
+    }
+    return loggers[name]
+}
 
 pm2.launchBus(function(err, bus) {
   bus.on('*', function(event, data){
@@ -15,10 +47,25 @@ pm2.launchBus(function(err, bus) {
   });
 
   bus.on('log:err', function(data) {
-    logger.error('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+    getLogger(data).error('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
   });
 
   bus.on('log:out', function(data) {
-    logger.log('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+    getLogger(data).log('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
   });
+   bus.emit("log:out", {
+     process: {
+       name: "proc1",
+       pm_id: "0"
+     },
+     data: "something"
+   })
+   bus.emit("log:err", {
+     process: {
+       name: "proc2",
+       pm_id: "1"
+     },
+     data: "something"
+   })
 });
+
